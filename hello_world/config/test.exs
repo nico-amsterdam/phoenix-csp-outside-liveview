84114ec3a14fa16f588179e6e249200317e7366a
Nico <n.g.c.hoogervorst@gmail.com@users.noreply.github.com>
2023-10-21 20:43:53 +0200

    mix archive.install hex phx_new; mix phx.new hello_world --database sqlite3

---
+++ hello_world/config/test.exs
@@ -0,0 +1,30 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :hello_world, HelloWorld.Repo,
+  database: Path.expand("../hello_world_test.db", Path.dirname(__ENV__.file)),
+  pool_size: 5,
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :hello_world, HelloWorldWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "knVCzof6gx6x8Bfn646VXziYrr5cmanehnJ1sED27S75u4k1RMJRn1uxiqhwodHc",
+  server: false
+
+# In test we don't send emails.
+config :hello_world, HelloWorld.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters.
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
