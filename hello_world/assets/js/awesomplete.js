51e8a782d5a281b4739c67071341a7a8e3df7aeb
Nico <n.g.c.hoogervorst@gmail.com@users.noreply.github.com>
2023-11-05 18:05:22 +0100

    Remove test

--- hello_world/assets/js/awesomplete.js
+++ hello_world/assets/js/awesomplete.js
@@ -1,15 +1,6 @@
 import Awesomplete from '../vendor/awesomplete-v2020.min.js'
 import AwesompleteUtil from '../vendor/awesomplete-util.min.js'
 
-
-let obj523 = { aa: 1 }
-let obj5234 = { ab: 2 }
-
-
 // expose Awesomplete
 window.Awesomplete = Awesomplete
 window.AwesompleteUtil = AwesompleteUtil
-
-window.obj523 = obj523
-self.obj5234 = obj5234
-
